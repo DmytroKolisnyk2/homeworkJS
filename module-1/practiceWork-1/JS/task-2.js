0585ac273cf02de00aa70114c362c9062ebd3e9b
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-01-25 19:55:51 +0200

    add homework

---
+++ module-1/practiceWork-1/JS/task-2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const str = "Привіт";
+const num = 123;
+const flag = true;
+const txt = "true";
+const numberPi = 3.14;
+let userAge = 12;
+let address = "st. Shevchenka 25";
+let favoriteColor = 'green';
+let findResult = null;
+let isAteist = false;
