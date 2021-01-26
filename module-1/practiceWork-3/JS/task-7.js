a61fc9db92a0cc8d879872b86b4e4f4725d56b60
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-01-26 09:30:31 +0200

    end homework 3

---
+++ module-1/practiceWork-3/JS/task-7.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("task-7");
+
+const a1 = 'My';
+const a2 = 'name';
+const a3 = 'is';
+let fullName = `${a1} ${a2} ${a3}`;
+fullName = `${fullName} Victor`;
+console.log(fullName);
---
+++ module-1/practiceWork-3/JS/task-8.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("task-8");
+
+const userName = 'Вадим';
+const payment = 50000;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
