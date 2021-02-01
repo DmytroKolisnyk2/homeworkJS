bec1611d4c85bbfbf4d5661a3c97243cd692609e
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-02-01 19:58:16 +0200

    fix error

--- module-1/practiceWork-4/JS/task-4.js
+++ module-1/practiceWork-4/JS/task-4.js
@@ -1,12 +1,15 @@
 "use strict";
 console.log("task-4");
 
-const random = Math.random() * 4 + 1;
-console.log(random);
-if (random < 2) {
-  console.log("Випало число менше 2");
-} else if (random > 2) {
-  console.log("Випало число більше 2");
-} else if ((random = 2)) {
+const random = Math.round(Math.random() * 4 + 1);
+if (random === 1) {
+  console.log("Випало число 1");
+} else if (random === 2) {
   console.log("Випало число 2");
+} else if (random === 3) {
+  console.log("Випало число 3");
+} else if (random === 4) {
+  console.log("Випало число 4");
+} else if (random === 5) {
+  console.log("Випало число 5");
 }
