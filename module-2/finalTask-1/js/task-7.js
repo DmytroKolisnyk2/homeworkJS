4f5c26bd72c989d3543071deba05d4cbe638de34
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-02-17 23:30:04 +0200

    fix error

--- module-2/finalTask-1/js/task-7.js
+++ module-2/finalTask-1/js/task-7.js
@@ -1,1 +1,29 @@
-console.log('task-1');
+console.log("task-7");
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const isLoginValid = function (login) {
+  let result;
+  login.length >= 4 && login.length <= 16 ? (result = true) : (result = false);
+  return result;
+};
+
+const isLoginUnique = function (allLogins, login) {
+  let result;
+  allLogins.includes(login) ? (result = false) : (result = true);
+  return result;
+};
+
+const addLogin = function (allLogins, login) {
+  if (!isLoginValid(login)) {
+    return console.log("Помилка! Логін повинен бути від 4 до 16 символів");
+  }
+  if (!isLoginUnique(allLogins, login)) {
+    return console.log('Такий логін уже використовується!');
+  }
+  return console.log('Логін успішно доданий!');
+};
+
+console.log(addLogin(logins, "Ajax"));
+console.log(addLogin(logins, "robotGoogles"));
+console.log(addLogin(logins, "Zod"));
+console.log(addLogin(logins, "jqueryisextremelyfast"));
