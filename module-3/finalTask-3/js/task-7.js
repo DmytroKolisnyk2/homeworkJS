3aed2e4b052cafdd12d8793410e492d6963c3532
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-02-28 22:51:26 +0200

    add task 7

--- module-3/finalTask-3/js/task-7.js
+++ module-3/finalTask-3/js/task-7.js
@@ -14,43 +14,53 @@ const account = {
     return createdTransaction;
   },
 
-  /*
-   * Метод відповідає за додавання суми до балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій
-   */
   deposit(amount) {
-    createTransaction(amount, "deposit");
+    if (+amount === NaN) {
+      return console.log("Ви ввели не число");
+    }
+    this.transactions.push(this.createTransaction(amount, "deposit"));
     this.balance += amount;
   },
 
-  /*
-   * Метод відповідає за зняття суми з балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій.
-   *
-   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
-   * про те, що зняття такої суми не можливо, недостатньо коштів.
-   */
-  withdraw(amount) {},
+  withdraw(amount) {
+    if (+amount === NaN) {
+      return console.log("Ви ввели не число");
+    }
+    if (amount > this.balance) {
+      return console.log("Не достатньо коштів на рахунку");
+    }
+    this.transactions.push(this.createTransaction(amount, "withdraw"));
+    this.balance -= amount;
+  },
 
-  /*
-   * Метод повертає поточний баланс
-   */
-  getBalance() {},
+  getBalance() {
+    return this.balance;
+  },
 
-  /*
-   * Метод шукає і повертає об'єкт транзакції по id
-   */
-  getTransactionDetails(id) {},
+  getTransactionDetails(id) {
+    return this.transactions[id];
+  },
 
-  /*
-   * Метод повертає кількість коштів
-   * певного типу транзакції з усієї історії транзакцій
-   */
-  getTransactionTotal(type) {},
+  getTransactionTotal(type) {
+    let result = 0;
+    for (const obj of this.transactions) {
+      const { currentAmount,currentType } = obj;
+      if (type === currentType) {
+        result += currentAmount;
+      }
+    }
+    return result;
+  },
 };
+account.deposit(324);
 account.deposit(12);
+account.withdraw(500);
+account.withdraw(122);
+account.withdraw(12);
+
 console.log(account.transactions);
+console.log(account.getBalance());
+console.log(account.getTransactionDetails(3));
+console.log(account.getTransactionTotal('deposit'));
+console.log(account.getTransactionTotal('withdraw'));
+
