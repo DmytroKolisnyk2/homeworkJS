4fd33ff0dd02511451720fa97830ca7ffce0b598
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-03-30 22:16:34 +0300

    fix bag

--- module-7/finalTask-7/js/task-06.js
+++ module-7/finalTask-7/js/task-06.js
@@ -13,21 +13,3 @@ const validation = () => {
   }
 };
 validationInputRef.addEventListener("blur", validation);
-
-// Не працює(((
-// const validationInputRef = document.querySelector("#validation-input");
-// const validationLength = +validationInputRef.dataset.length;
-// validationInputRef.placeholder = `Введи ${validationLength} символів`;
-// console.log(validationLength);
-// const validation = function(value) {
-//   console.log(validationInputRef.value.length);
-//   if (validationInputRef.value.length !== value) {
-//     validationInputRef.classList.remove("valid");
-//     validationInputRef.classList.add("invalid");
-//   }
-//   if (validationInputRef.value.length === value) {
-//     validationInputRef.classList.remove("invalid");
-//     validationInputRef.classList.add("valid");
-//   }
-// };
-// validationInputRef.addEventListener("input", validation(validationLength));
