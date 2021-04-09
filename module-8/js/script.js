9fd61b2ad13bb20a37d90ec5500c1c9b707ffc2e
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-04-10 00:43:43 +0300

    remove injasent html

--- module-8/js/script.js
+++ module-8/js/script.js
@@ -9,40 +9,21 @@ const refs = {
   closeAreaRef: document.querySelector(".lightbox__overlay"),
   galleryRef: document.querySelector(".js-gallery"),
 };
-photos.forEach((photo) =>
-  refs.galleryRef.insertAdjacentHTML(
-    "beforeend",
-    `<li class="gallery__item">
-     <a
-    class="gallery__link"
-    href="${photo.original}"
-  >
-    <img
-      class="gallery__image"
-      src="${photo.preview}"
-      data-id='${photo.id}'
-      data-source="${photo.original}"
-      alt="${photo.description}"
-    />
-  </a>
-</li>`
-  )
-);
-// const removeModal = () => {
-//   refs.lightboxRef.classList.remove("is-open");
-//   refs.closeAreaRef.removeEventListener("click", removeModal);
-//   refs.closeButtonRef.removeEventListener("click", removeModal);
-//   window.removeEventListener("keydown", removeModalEsc);
-// };
-// const removeModalEsc = (event) => {
-//   console.log(event.key);
-//   if (event.key === "Escape") {
-//     refs.lightboxRef.classList.remove("is-open");
-//     refs.closeAreaRef.removeEventListener("click", removeModal);
-//     refs.closeButtonRef.removeEventListener("click", removeModal);
-//     window.removeEventListener("keydown", removeModalEsc);
-//   }
-// };
+photos.forEach((photo) => {
+  const item = document.createElement("li");
+  const link = document.createElement("a");
+  const image = document.createElement("img");
+  item.classList.add("gallery__item");
+  link.classList.add("gallery__link");
+  image.classList.add("gallery__image");
+  image.setAttribute("data-id", photo.id);
+  image.setAttribute("data-source", photo.original);
+  link.href = photo.original;
+  image.src = photo.preview;
+  refs.galleryRef.appendChild(item);
+  item.appendChild(link);
+  link.appendChild(image);
+});
 refs.photosRef = [...refs.galleryRef.querySelectorAll(".gallery__image")];
 refs.galleryRef.addEventListener("click", (event) => {
   refs.originalImageRef.src = event.target.dataset.source;
@@ -56,6 +37,4 @@ refs.galleryRef.addEventListener("click", (event) => {
   window.addEventListener("keydown", refs.bindRemoveModalEsc);
   window.addEventListener("keydown", refs.bindChangePhoto);
   refs.closeAreaRef.addEventListener("click", refs.bindRemoveModal);
-  console.log(refs);
 });
-// console.log(document.querySelector('[data-id="5"]').src);
