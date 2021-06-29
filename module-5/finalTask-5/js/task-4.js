237c65e173bd482a659c1b093090ae3b0455cc4f
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-06-29 17:11:08 +0300

    add homework

---
+++ module-5/finalTask-5/js/task-4.js
@@ -0,0 +1,27 @@
+console.log("task-4");
+
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return this._value
+  }
+  set value(value) {
+    this._value = value;
+  }
+  append(str) { this.value = this.value + str }
+  prepend(str) { this.value = str + this.value }
+  pad(str) { this.value = str + this.value + str }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
