e47dc757ff6cf45976fb05bdb3dc5bf9bd384e75
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-08-12 15:33:48 +0300

    add timer

--- module-11/timer/src/index.js
+++ module-11/timer/src/index.js
@@ -1,28 +1,36 @@
 import './sass/main.scss';
-import Tick from '@pqina/flip';
+class CountdownTimer {
+  constructor({ targetDate, timerRef }) {
+    this.targetDate = targetDate;
+    this.timerRef = document.querySelector(`${timerRef}`);;
+    this.days = this.timerRef.querySelector('[data-value="days"]');
+    this.hours = this.timerRef.querySelector('[data-value="hours"]');
+    this.mins = this.timerRef.querySelector('[data-value="mins"]');
+    this.secs = this.timerRef.querySelector('[data-value="secs"]');
+  }
+  start() {
+    this.updateClockFace(0);
+    const startTime = this.targetDate; 
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
 
-function setupFlip(tick) {
+      this.updateClockFace(deltaTime);
+    }, 1000);
+  }
+  updateClockFace(time) {
+    this.days.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    this.hours.textContent = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    this.mins.textContent = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    this.secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+const timer = new CountdownTimer({
+  timerRef: '#timer-1',
+  targetDate: new Date('Sep 17, 2021'),
+});
 
-  Tick.helper.interval(function () {
-
-    tick.value++;
-
-    // Set `aria-label` attribute which screenreaders will read instead of HTML content
-    tick.root.setAttribute('aria-label', tick.value);
-
-  }, 1000);
-
-}     
-console.log(Tick);
-// // const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-// // const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-// // const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-// // const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-// // new CountdownTimer({
-// //   selector: '#timer-1',
-// //   targetDate: new Date('Jul 17, 2019'),
-// // });
+timer.start();
