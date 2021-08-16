c784f555da6016cb8278b536fde8b7a7b5238a67
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-08-16 15:22:53 +0300

    remove comment

--- module-11/promisification/js/task-1.js
+++ module-11/promisification/js/task-1.js
@@ -11,14 +11,3 @@ const logger = time => console.log(`%c task-1: %cResolved after ${time}ms` , 'co
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
-
-// const getMessage = function () {
-//   const input = prompt('Введіть повідомлення');
-
-//   return Promise.resolve(input);
-// };
-
-// const logger = message => console.log(message);
-
-// // Або ще коротше
-// getMessage().then(logger);
