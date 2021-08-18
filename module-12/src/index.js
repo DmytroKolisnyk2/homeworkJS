77621cb64ef35f32eec62d07cb2f7168b1435364
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-08-18 17:53:35 +0300

    add homework

---
+++ module-12/src/index.js
@@ -0,0 +1,41 @@
+import './sass/main.scss';
+import cardTemplate from './templates/card.hbs';
+import searchTemplate from './templates/search.hbs';
+const debounce = require('lodash.debounce');
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+
+
+const containerRef = document.querySelector('.container');
+const inputRef = document.querySelector('.input');
+const URL = 'https://restcountries.eu/rest/v2';
+const searchCountry = (event) => {
+  fetch(`${URL}/name/${event.target.value}?fields=name;capital;flag;languages;population`)
+    .then((data) => data.json())
+    .then(
+      (data) => {
+        if (data.length >= 10) {
+          error({
+            text: 'Too many matches found. Please enter a more specific query!',
+            delay: 1000
+          })
+          containerRef.innerHTML = '';
+        };
+        if (1 < data.length && data.length <= 10) {
+          containerRef.innerHTML = searchTemplate(data);
+          containerRef.addEventListener('click', (ev) => {
+            if (!ev.target.classList.contains('search__item')) return;
+            inputRef.value = ev.target.textContent;
+            containerRef.innerHTML = cardTemplate(data.find(country => country.name === ev.target.textContent));
+          });
+        };
+        if (data.length === 1) containerRef.innerHTML = cardTemplate(...data);
+      })
+    // .catch(error({
+    //   text: 'Query not found',
+    //   delay: 500
+    // }))
+};
+inputRef.addEventListener('input', debounce(searchCountry, 500));
